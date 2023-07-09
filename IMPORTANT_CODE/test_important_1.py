c2f0577075318e77402fc12d0fa2b46f071a44ad
Sameer <nischalkarn369369@gmail.com>
2023-07-10 00:44:52 +0300

    Terminal query updated

--- IMPORTANT_CODE/test_important_1.py
+++ IMPORTANT_CODE/test_important_1.py
@@ -4,8 +4,14 @@ import subprocess
 terminal = subprocess.Popen(['powershell', '-NoExit'], stdin=subprocess.PIPE, creationflags=subprocess.CREATE_NEW_CONSOLE)
 
 while True:
-    msg = input("askmessage: ")
+    msg = input("Enter Message:> ")
     print(msg)
+
+    if msg == "quit":
+        # Close the terminal and exit the program
+        terminal.stdin.close()
+        terminal.terminate()
+        break
 
     # Print the user-entered message in the terminal
     terminal.stdin.write(f'Write-Host "{msg}"\n'.encode())
